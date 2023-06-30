e36f4c7b40b0503f8fbbc7333807282dd0a30faa
sindhushakoppisetty <96305586+sindhushakoppisetty@users.noreply.github.com>
2023-06-30 15:32:29 +0530

    import math a,b=map(int,input().split()) s=math.gcd(a,b) print(s)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,15 +1,4 @@
-def hcf(a,b):
-    t=2
-    res=1
-    while a>=t and b>=t:
-         if a%t==0 and b%t==0:
-            a=a//t
-            b=b//t
-            res=res*t
-         else:
-             t+=1
-    return res
-
+import math
 a,b=map(int,input().split())
-
-print(hcf(a,b))
+s=math.gcd(a,b)
+print(s)
