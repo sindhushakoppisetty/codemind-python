e469cadb13e495f9410e3af12d10ceed2abdc8ba
sindhushakoppisetty <96305586+sindhushakoppisetty@users.noreply.github.com>
2023-06-30 22:10:55 +0530

    r,c=map(int,input().split()) mat=[] for i in range(r):     b=list(map(int,input().split()))     mat.append(b) rsum=[] for i in range(r):     rsum.append(sum(mat[i])) print(max(rsum))

---
+++ Maximum_sum_of_row_wise_sum_of_a_matrix.py
@@ -0,0 +1,9 @@
+r,c=map(int,input().split())
+mat=[]
+for i in range(r):
+    b=list(map(int,input().split()))
+    mat.append(b)
+rsum=[]
+for i in range(r):
+    rsum.append(sum(mat[i]))
+print(max(rsum))
