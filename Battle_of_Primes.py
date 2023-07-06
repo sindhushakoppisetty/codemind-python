ae80d467a8813b2dd022e5a2441881b70a4d2179
sindhushakoppisetty <96305586+sindhushakoppisetty@users.noreply.github.com>
2023-07-06 15:43:50 +0530

    import math def prime(n):     if n==1:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True n=int(input()) m=int(input()) c=n+m for i in range(1,20):     if prime(c+i):         print(i)         break

---
+++ Battle_of_Primes.py
@@ -0,0 +1,16 @@
+import math
+def prime(n):
+    if n==1:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+m=int(input())
+c=n+m
+for i in range(1,20):
+    if prime(c+i):
+        print(i)
+        break
+        
